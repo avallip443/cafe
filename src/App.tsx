3ad929c85fd0dbdbbadbd7164576d4610c01970c
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-03-08 01:19:23 -0500

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.css";
 import Home from "./pages/Home";
-import Header from "./shared/Header"
-import Footer from "./shared/Footer"
-import Menu from './pages/Menu';
-import About from './pages/About';
+import Header from "./shared/Header";
+import Footer from "./shared/Footer";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <About />
-      {/*<Home />
-      <Menu />*/}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
