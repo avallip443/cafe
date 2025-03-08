144f28d10cd96d4819510df9216f59838724666e
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-03-08 12:31:14 -0500

    update nav

--- src/components/menu_bar/MenuBar.tsx
+++ src/components/menu_bar/MenuBar.tsx
@@ -13,15 +13,15 @@ export default function MenuSidebar() {
       {isOpen && (
         <div className="fixed top-0 left-0 h-full w-1/4 bg-(--beige) shadow-lg transform transition-transform duration-300 text-black">
           <div className="flex justify-between items-center p-4 border-b">
-            <h2 className="text-xl font-semibold">cafe grenouille</h2>
-            <button onClick={() => setIsOpen(!isOpen)}>X</button>
+            <h2 className="text-xl font-semibold text-center"><a href="/">cafe grenouille</a></h2>
+            <button className="text-xl hover:text-(--green-accent) hover:font-bold" onClick={() => setIsOpen(!isOpen)}>X</button>
           </div>
 
           <div className="py-12">
-            <ul className="space-y-4">
-              <li className="text-lg"><a href="/">home</a></li>
-              <li className="text-lg"><a href="/menu">menu</a></li>
-              <li className="text-lg"><a href="/about">about</a></li>
+            <ul className="space-y-5">
+              <li className="text-lg hover:text-(--green-accent) hover:font-semibold"><a href="/">home</a></li>
+              <li className="text-lg hover:text-(--green-accent) hover:font-semibold"><a href="/menu">menu</a></li>
+              <li className="text-lg hover:text-(--green-accent) hover:font-semibold"><a href="/about">about</a></li>
             </ul>
           </div>
         </div>
