a2c3bd576d19eee0ba40f7fd39bf50a6660a5370
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-03-08 23:59:54 -0500

    add bg menu decals

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MenuList from "../utils/menu.json"
+import MenuList from "../utils/menu.json";
 
 type MenuItem = {
   name: string;
@@ -13,61 +13,98 @@ type MenuItem = {
 const Menu = () => {
   return (
     <div className="p-6 max-w-3xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 bg-(--yellow) text-black">brunch</h1>
+      <img
+        src="/designs/coffee_cup_decal.png"
+        className="absolute top-100 left-3 -translate-y-1/2 -z-10 w-96"
+      />
+      <img
+        src="/designs/croissant_decal.png"
+        className="absolute top-350 right-8 -translate-y-1/2 -z-10 w-64 md:w-80"
+      />
+
+      <img
+        src="/designs/people_decal.png"
+        className="absolute top-700 left-3 -z-10 w-96"
+      />
+
+      <h1 className="text-xl md:text-3xl font-bold mb-4 bg-(--yellow) text-black">
+        brunch
+      </h1>
       {MenuList.brunch.map((section, index) => (
         <div key={index}>
           <p className="italic text-gray-600 mb-2">{section.times}</p>
 
-          <h2 className="text-2xl font-semibold mt-5 text-(--green-accent)">starters</h2>
+          <h2 className="text-lg md:text-2xl font-semibold mt-5 text-(--green-accent)">
+            starters
+          </h2>
           {section.starters.map((item, idx) => (
             <MenuItemCard key={idx} item={item} />
           ))}
 
-          <h2 className="text-2xl font-semibold mt-5 text-(--green-accent)">salads</h2>
+          <h2 className="text-lg md:text-2xl font-semibold mt-5 text-(--green-accent)">
+            salads
+          </h2>
           {section.salads.map((item, idx) => (
             <MenuItemCard key={idx} item={item} />
           ))}
 
-          <h2 className="text-2xl font-semibold mt-5 text-(--green-accent)">mains</h2>
+          <h2 className="text-lg md:text-2xl font-semibold mt-5 text-(--green-accent)">
+            mains
+          </h2>
           {section.mains.map((item, idx) => (
             <MenuItemCard key={idx} item={item} />
           ))}
 
-          <h2 className="text-2xl font-semibold mt-5 text-(--green-accent)">sides</h2>
+          <h2 className="text-lg md:text-2xl font-semibold mt-5 text-(--green-accent)">
+            sides
+          </h2>
           {section.sides.map((item, idx) => (
             <MenuItemCard key={idx} item={item} />
           ))}
         </div>
       ))}
 
-      <h1 className="text-3xl font-bold mt-8 bg-(--yellow) text-black">pastries</h1>
+      <h1 className="text-xl md:text-3xl font-bold mt-8 bg-(--yellow) text-black">
+        pastries
+      </h1>
       {MenuList.pastries.map((item, idx) => (
         <MenuItemCard key={idx} item={item} />
       ))}
 
-      <h1 className="text-3xl font-bold mt-8 bg-(--yellow) text-black">drinks</h1>
+      <h1 className="text-xl md:text-3xl font-bold mt-8 bg-(--yellow) text-black">
+        drinks
+      </h1>
       {MenuList.drinks.map((item, idx) => (
         <MenuItemCard key={idx} item={item} />
       ))}
-      
-      <h1 className="text-3xl font-bold mt-8 bg-(--yellow) text-black">weekly specials</h1>
-      <p className="text-gray-600 text-sm mt-4">please visit us to find out about your weekly special - a new pastry or drink!</p>
+
+      <h1 className="text-xl md:text-3xl font-bold mt-8 bg-(--yellow) text-black">
+        weekly specials
+      </h1>
+      <p className="text-gray-600 text-xs md:text-sm mt-4">
+        please visit us to find out about our weekly special - a new pastry or
+        drink!
+      </p>
     </div>
   );
 };
 
 const MenuItemCard = ({ item }: { item: MenuItem }) => (
   <div className="pt-2">
-    <h3 className="text-lg font-medium text-black mt-2">{item.name} - ${item.price}</h3>
-    <p className="text-gray-600 text-sm">{item.description}</p>
-    {item.diet && <p className="text-sm italic text-green-600">{item.diet}</p>}
+    <h3 className="text-sm md:text-lg font-medium text-black mt-2">
+      {item.name} - ${item.price}
+    </h3>
+    <p className="text-gray-600 text-xs md:text-sm">{item.description}</p>
+    {item.diet && (
+      <p className="text-xs md:text-sm italic text-green-600">{item.diet}</p>
+    )}
     {item.milk_options && (
-      <p className="text-sm italic text-blue-600">
+      <p className="text-xs md:text-sm italic text-blue-600">
         milk options: {item.milk_options.join(", ")}
       </p>
     )}
     {item.add_ons && (
-      <p className="text-sm italic text-blue-600">
+      <p className="text-xs md:text-sm italic text-blue-600">
         Add-ons:{" "}
         {Object.entries(item.add_ons)
           .map(([addon, price]) => `${addon} +$${price}`)
