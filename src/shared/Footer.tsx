d82d2b8eb59257fe9198e2b2b125a76cb25440c7
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-03-09 22:39:46 -0400

    create footer

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div id="footer" className="mt-12 bg-black h-24 flex justify-center items-center">
-      <h1 className="text-white">cafe grenouille</h1>
+    <div
+      id="footer"
+      className="mt-12 bg-black h-24 flex flex-col justify-center items-center text-white"
+    >
+      <p>cafe grenouille</p>
+      <p>123 react rd, toronto</p>
+      <p>(123)-456-7890)</p>
+
     </div>
   );
 };
