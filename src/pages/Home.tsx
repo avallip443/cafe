2e53eefbc2bdc80cfa54773f4c0b7abbec5dece6
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2025-03-10 21:02:21 -0400

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,21 +19,21 @@ const Home: React.FC = () => {
       <div className="max-w-6xl flex flex-col items-center justify-center">
         <motion.div
           id="landing-image"
-          className="w-screen h-64 md:h-152 bg-cover bg-center bg-[url('images/cafe_landing1.jpg')] flex flex-col justify-end items-center md:items-end pb-4 md:pb-10 md:pr-10"
+          className="w-screen h-82 md:h-screen bg-cover bg-center bg-[url('images/cafe_landing1.jpg')] flex flex-col justify-end items-end pb-4 md:pb-16"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
           <motion.p
-            className="text-lg md:text-5xl bg-green-800/80 p-4 rounded-xl"
+            className="text-xl md:text-7xl bg-(--green-accent)/60 w-55 md:w-200 pr-6 md:pr-12 py-2 md:py-5 rounded-l-lg"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.7 }}
           >
-            parisan brunch and cafe
+            cafe grenouille
           </motion.p>
           <motion.p
-            className="text-xs md:text-2xl"
+            className="text-xs md:text-3xl w-2/3 text-right md:w-full mt-2 md:mt-4 mr-2 md:mr-12"
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.9, delay: 0.2 }}
@@ -51,8 +51,10 @@ const Home: React.FC = () => {
           viewport={{ once: true }}
         >
           <motion.div
-            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-cover bg-center"
-            style={{ backgroundImage: `url(/images/brunch_${currentIndex}.png)` }}
+            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-cover bg-center rounded-2xl"
+            style={{
+              backgroundImage: `url(/images/brunch_${currentIndex}.png)`,
+            }}
             initial={{ opacity: 0, x: -30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.7 }}
@@ -60,15 +62,21 @@ const Home: React.FC = () => {
           ></motion.div>
 
           <motion.div
-            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-(--green) text-black flex flex-col justify-center items-center p-12"
+            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-(--green) text-black flex flex-col justify-center items-center p-12 rounded-2xl"
             initial={{ opacity: 0, x: 30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.7 }}
             viewport={{ once: true }}
           >
-            <h5 className="text-xl md:text-2xl font-semibold mb-6">cafe grenouille</h5>
-            <p className="mb-4 text-sm md:text-lg">123 react rd, toronto, canada</p>
-            <p className="font-medium underline text-sm md:text-lg">tuesday to sunday, 8am-4pm</p>
+            <h5 className="text-xl md:text-2xl font-semibold mb-6">
+              cafe grenouille
+            </h5>
+            <p className="mb-4 text-sm md:text-lg">
+              123 react rd, toronto, canada
+            </p>
+            <p className="font-medium underline text-sm md:text-lg">
+              tuesday to sunday, 8am-4pm
+            </p>
           </motion.div>
         </motion.div>
 
@@ -81,40 +89,55 @@ const Home: React.FC = () => {
           viewport={{ once: true }}
         >
           <motion.div
-            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-(--yellow) text-black flex flex-col justify-center items-center p-12"
+            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-(--yellow) text-black flex flex-col justify-center items-center p-12 rounded-2xl"
             initial={{ opacity: 0, x: -30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6 }}
             viewport={{ once: true }}
           >
             <h5 className="text-xl md:text-2xl font-semibold mb-6">menu</h5>
-            <p className="mb-4 text-sm md:text-lg">visit us in person and discover our weekly special!</p>
-            <a href="/menu" className="underline font-medium hover:font-bold text-sm md:text-lg">
+            <p className="mb-4 text-sm md:text-lg">
+              visit us in person and discover our weekly special!
+            </p>
+            <a
+              href="/menu"
+              className="underline font-medium hover:font-bold text-sm md:text-lg"
+            >
               view our cafe and brunch menu
             </a>
           </motion.div>
 
           <motion.div
-            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-cover bg-center"
-            style={{ backgroundImage: `url(/images/coffee_${currentIndex}.png)` }}
+            className="h-64 md:h-150 w-[88vw] md:w-1/2 bg-cover bg-center rounded-2xl"
+            style={{
+              backgroundImage: `url(/images/coffee_${currentIndex}.png)`,
+            }}
             initial={{ opacity: 0, x: 30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6 }}
             viewport={{ once: true }}
           ></motion.div>
         </motion.div>
-
-        <Reservations />
-
         <motion.div
           id="reviews"
-          className="mt-16 md:mt-32 justify-center md:flex"
+          className="justify-center md:flex"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
           viewport={{ once: true }}
         >
-          <div className="flex flex-col md:flex-row text-black md:h-64 w-[88vw] md:w-full">
+          <Reservations />
+        </motion.div>
+
+        <motion.div
+          id="reviews"
+          className="justify-center md:flex"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          viewport={{ once: true }}
+        >
+          <div className="flex flex-col md:flex-row text-black md:h-64 w-[88vw] md:w-full gap-3">
             {[
               {
                 text: `"Cafe Grenouille is an absolute gem in the heart of Toronto!"`,
@@ -131,14 +154,23 @@ const Home: React.FC = () => {
             ].map((review, index) => (
               <motion.div
                 key={index}
-                className="md:w-1/3 flex flex-col items-center justify-evenly text-(--green-accent) hover:text-black border border-(--green-accent) px-4 py-6 hover:bg-(--yellow) cursor-pointer"
+                className="md:w-1/3 flex flex-col items-center justify-evenly text-(--green-accent) hover:text-black border border-(--green-accent) px-4 py-6 hover:bg-(--yellow) cursor-pointer rounded-2xl"
                 initial={{ opacity: 0, scale: 0.9 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.5, delay: index * 0.2 }}
                 viewport={{ once: true }}
               >
-                <p className="text-sm md:text-lg font-semibold">{review.text}</p>
-                <img src={review.logo} className="mt-4 w-16 md:w-22" />
+                <p className="text-sm md:text-lg font-semibold">
+                  {review.text}
+                </p>
+                <img
+                  src={review.logo}
+                  className={`mt-4 ${
+                    review.logo.includes("globe")
+                      ? "h-4 md:h-5"
+                      : "w-22 md:2-26"
+                  }`}
+                />
               </motion.div>
             ))}
           </div>
